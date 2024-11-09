fe62d5eeb6423b09eff9b40a334d9aefd15fbfdf
aAlzawri <A.Alzawri@gmail.com>
2024-11-09 19:58:50 +0300

    1

--- src/Octoshift/Services/ArchiveUploader.cs
+++ src/Octoshift/Services/ArchiveUploader.cs
@@ -15,7 +15,6 @@ public class ArchiveUploader
     private readonly GithubClient _client;
     private readonly OctoLogger _log;
     internal int _streamSizeLimit = 100 * 1024 * 1024; // 100 MiB
-
     private const string BASE_URL = "https://uploads.github.com";
 
     public ArchiveUploader(GithubClient client, OctoLogger log)
@@ -23,6 +22,7 @@ public class ArchiveUploader
         _client = client;
         _log = log;
     }
+
     public virtual async Task<string> Upload(Stream archiveContent, string archiveName, string orgDatabaseId)
     {
         if (archiveContent == null)
@@ -30,56 +30,35 @@ public class ArchiveUploader
             throw new ArgumentNullException(nameof(archiveContent), "The archive content stream cannot be null.");
         }
 
-        using var streamContent = new StreamContent(archiveContent);
-        streamContent.Headers.ContentType = new("application/octet-stream");
+        var isMultipart = archiveContent.Length > _streamSizeLimit;
+        var url = $"{BASE_URL}/organizations/{orgDatabaseId.EscapeDataString()}/gei/archive" +
+                  (isMultipart ? "/blobs/uploads" : $"?name={archiveName.EscapeDataString()}");
 
-        var isMultipart = archiveContent.Length > _streamSizeLimit; // Determines if stream size is greater than 100MB
+        return isMultipart ? await UploadMultipart(archiveContent, archiveName, url)
+                           : await UploadSingle(archiveContent, url);
+    }
 
-        string response;
-
-        if (isMultipart)
-        {
-            var url = $"{BASE_URL}/organizations/{orgDatabaseId.EscapeDataString()}/gei/archive/blobs/uploads";
-
-            response = await UploadMultipart(archiveContent, archiveName, url);
-            return response;
-        }
-        else
-        {
-            var url = $"{BASE_URL}/organizations/{orgDatabaseId.EscapeDataString()}/gei/archive?name={archiveName.EscapeDataString()}";
-
-            response = await _client.PostAsync(url, streamContent);
-            var data = JObject.Parse(response);
-            return (string)data["uri"];
-        }
+    private async Task<string> UploadSingle(Stream archiveContent, string url)
+    {
+        using var streamContent = CreateStreamContent(archiveContent);
+        var response = await _client.PostAsync(url, streamContent);
+        return (string)JObject.Parse(response)["uri"];
     }
 
     private async Task<string> UploadMultipart(Stream archiveContent, string archiveName, string uploadUrl)
     {
         var buffer = new byte[_streamSizeLimit];
+        var guid = string.Empty;
 
         try
         {
-            // 1. Start the upload
             var startHeaders = await StartUpload(uploadUrl, archiveName, archiveContent.Length);
-
             var nextUrl = GetNextUrl(startHeaders);
 
-            var guid = HttpUtility.ParseQueryString(nextUrl.Query)["guid"];
+            guid = HttpUtility.ParseQueryString(nextUrl.Query)["guid"];
+            nextUrl = await UploadParts(buffer, archiveContent, nextUrl);
 
-            // 2. Upload parts
-            int bytesRead;
-            var partsRead = 0;
-            var totalParts = (long)Math.Ceiling((double)archiveContent.Length / _streamSizeLimit);
-            while ((bytesRead = await archiveContent.ReadAsync(buffer)) > 0)
-            {
-                nextUrl = await UploadPart(buffer, bytesRead, nextUrl.ToString(), partsRead, totalParts);
-                partsRead++;
-            }
-
-            // 3. Complete the upload
             await CompleteUpload(nextUrl.ToString());
-
             return $"gei://archive/{guid}";
         }
         catch (Exception ex)
@@ -88,16 +67,26 @@ public class ArchiveUploader
         }
     }
 
+    private async Task<Uri> UploadParts(byte[] buffer, Stream archiveContent, Uri nextUrl)
+    {
+        int bytesRead;
+        var partsRead = 0;
+        var totalParts = (long)Math.Ceiling((double)archiveContent.Length / _streamSizeLimit);
+
+        while ((bytesRead = await archiveContent.ReadAsync(buffer)) > 0)
+        {
+            nextUrl = await UploadPart(buffer, bytesRead, nextUrl.ToString(), partsRead, totalParts);
+            partsRead++;
+        }
+
+        return nextUrl;
+    }
+
     private async Task<IEnumerable<KeyValuePair<string, IEnumerable<string>>>> StartUpload(string uploadUrl, string archiveName, long contentSize)
     {
         _log.LogInformation($"Starting archive upload into GitHub owned storage: {archiveName}...");
 
-        var body = new
-        {
-            content_type = "application/octet-stream",
-            name = archiveName,
-            size = contentSize
-        };
+        var body = new { content_type = "application/octet-stream", name = archiveName, size = contentSize };
 
         try
         {
@@ -113,15 +102,11 @@ public class ArchiveUploader
     private async Task<Uri> UploadPart(byte[] body, int bytesRead, string nextUrl, int partsRead, long totalParts)
     {
         _log.LogInformation($"Uploading part {partsRead + 1}/{totalParts}...");
-        using var content = new ByteArrayContent(body, 0, bytesRead);
-        content.Headers.ContentType = new("application/octet-stream");
+        using var content = CreateStreamContent(body, bytesRead);
 
         try
         {
-            // Make the PATCH request and retrieve headers
             var (_, headers) = await _client.PatchWithFullResponseAsync(nextUrl, content);
-
-            // Retrieve the next URL from the response headers
             return GetNextUrl(headers.ToList());
         }
         catch (Exception ex)
@@ -143,19 +128,15 @@ public class ArchiveUploader
         }
     }
 
+    private StreamContent CreateStreamContent(Stream content) => new(content) { Headers = { ContentType = new("application/octet-stream") } };
+
+    private StreamContent CreateStreamContent(byte[] buffer, int length) => new(buffer, 0, length) { Headers = { ContentType = new("application/octet-stream") } };
+
     private Uri GetNextUrl(IEnumerable<KeyValuePair<string, IEnumerable<string>>> headers)
     {
-        // Use FirstOrDefault to safely handle missing Location headers
-        var locationHeader = headers.First(header => header.Key.Equals("Location", StringComparison.OrdinalIgnoreCase));
+        var locationHeader = headers.FirstOrDefault(header => header.Key.Equals("Location", StringComparison.OrdinalIgnoreCase));
+        var locationValue = locationHeader.Value?.FirstOrDefault();
 
-        if (!string.IsNullOrEmpty(locationHeader.Key))
-        {
-            var locationValue = locationHeader.Value.FirstOrDefault();
-            if (locationValue.HasValue())
-            {
-                return new Uri(new Uri(BASE_URL), locationValue);
-            }
-        }
-        throw new OctoshiftCliException("Location header is missing in the response, unable to retrieve next URL for multipart upload.");
+        return locationValue.HasValue() ? new Uri(new Uri(BASE_URL), locationValue) : throw new OctoshiftCliException("Location header is missing in the response.");
     }
 }
